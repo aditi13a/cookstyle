7dd74679b4dc870a7ae8ac4730fedb4b602d80de
Aditi <110167489+aditi13a@users.noreply.github.com>
2025-07-27 02:16:56 +0530

    Update use_node_normal_spec.rb

--- spec/rubocop/cop/chef/style/use_node_normal_spec.rb
+++ spec/rubocop/cop/chef/style/use_node_normal_spec.rb
@@ -1,25 +1,22 @@
-# frozen_string_literal: true
 require 'spec_helper'
 
-describe RuboCop::Cop::Chef::Style::UseNodeNormal do
-  subject(:cop) { described_class.new }
-
+RSpec.describe RuboCop::Cop::Chef::Style::UseNodeNormal, :rubocop do
   it 'registers an offense when using node.normal' do
     expect_offense(<<~RUBY)
       node.normal['foo'] = 'bar'
-           ^^^^^^ Avoid using `node.normal`. It persists data across Chef runs and is discouraged. Use `node.default` or `node.override` instead.
-    RUBY
-
-    expect_correction(<<~RUBY)
-      node.default['foo'] = 'bar'
+           ^^^^^^^ Chef/Style/UseNodeNormal: Avoid using `node.normal`. It persists data across Chef runs and is discouraged. Use `node.default` or `node.override` instead.
     RUBY
   end
 
   it 'does not register an offense when using node.default' do
-    expect_no_offenses("node.default['foo'] = 'bar'")
+    expect_no_offenses(<<~RUBY)
+      node.default['foo'] = 'bar'
+    RUBY
   end
 
   it 'does not register an offense when using node.override' do
-    expect_no_offenses("node.override['foo'] = 'bar'")
+    expect_no_offenses(<<~RUBY)
+      node.override['foo'] = 'bar'
+    RUBY
   end
 end
