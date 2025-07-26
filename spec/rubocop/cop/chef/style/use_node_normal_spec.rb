ea0e90e921c4ea676dde3e1c9818b689e9474e00
Aditi <110167489+aditi13a@users.noreply.github.com>
2025-07-27 02:39:24 +0530

    Update use_node_normal_spec.rb

--- spec/rubocop/cop/chef/style/use_node_normal_spec.rb
+++ spec/rubocop/cop/chef/style/use_node_normal_spec.rb
@@ -3,20 +3,6 @@ require 'spec_helper'
 describe RuboCop::Cop::Chef::Style::UseNodeNormal do
   subject(:cop) { described_class.new }
 
-it 'registers an offense when using node.normal' do
-  source = "node.normal['foo'] = 'bar'"
-  processed_source = parse_source(source)
-  _investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
-  offenses = _investigator.investigate(processed_source).offenses
-
-  offense = offenses.first
-  expect(offense.message).to eq(
-    'Avoid using `node.normal`. It persists data across Chef runs and is discouraged. Use `node.default` or `node.override` instead.'
-  )
-end
-
-
-
   it 'autocorrects node.normal to node.default' do
     corrected = autocorrect_source("node.normal['foo'] = 'bar'")
     expect(corrected).to eq("node.default['foo'] = 'bar'")
